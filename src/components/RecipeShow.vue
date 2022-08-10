3227d755477c9a674489ad9903ada7113448840d
mrshiam <mrshiam01@gmail.com>
2022-08-10 14:09:17 +0600

    changes in css

--- src/components/RecipeShow.vue
+++ src/components/RecipeShow.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <div class="grid gap-8 justify-items-center grid-cols-3 mt-8">
     <div v-for="recipe in recipes">
-        <img :src="recipe.image" alt="">
+        <img class="rounded-xl" :src="recipe.image" alt="">
         <h3 class="text-center mt-2"><a :href="recipe.url">{{ recipe.label }}</a></h3>
     </div>
   </div>
